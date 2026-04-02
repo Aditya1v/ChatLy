ae7dc5a64c61e8817b050cee72662ca5a3c94f89
Aditya <adivermapro@gmail.com>
2026-04-02 22:40:41 +0530

    fix api endpoint

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,6 @@
-export const URL = import.meta.env.VITE_API_KEY;
+export const API_KEY = import.meta.env.VITE_API_KEY;
+
+export const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${API_KEY}`;
 
 
 
--- src/services/chatService.js
+++ src/services/chatService.js
@@ -1,11 +1,17 @@
-import { URL } from "../constants/constants";
+import { API_URL } from "../constants/constants";
 
 export const fetchChatResponse = async (payload) => {
-  let res = await fetch(URL, {
+  let res = await fetch(API_URL, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify(payload),
   });
 
   res = await res.json();
+  if (!res.candidates) {
+    throw new Error("Invalid API response");
+  }
   return res.candidates[0].content.parts[0].text;
 };
